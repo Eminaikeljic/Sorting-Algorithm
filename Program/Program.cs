681da78c3aa074ca4c2c9a3e7cfe59b9bf7386bf
Mina <eminaikeljic@student.kaplan.edu>
2018-02-18 14:07:45 -0600

    Midsize Data Set

--- Program/Program.cs
+++ Program/Program.cs
@@ -7,7 +7,7 @@ namespace Program
     {
         static void Main(string[] args)
         {
-            var entries = new Int32[] { 8, 4, 5, 7, 1, 9, 6, 3, 2};
+            var entries = new Int32[] { 8, 4, 5, 7, 1, 9, 6, 3, 2, 8, 4, 5, 7, 1, 9, 6, 3, 2, 8, 4, 5, 7, 1, 9, 6, 3, 2, 8, 4, 5, 7, 1, 9, 6, 3, 2, 8, 4, 5, 7, 1, 9, 6, 3, 2, 8, 4, 5, 7, 1, 9, 6, 3, 2, 8, 4, 5, 7, 1, 9, 6, 3, 2, 8, 4, 5, 7, 1, 9, 6, 3, 2, 8, 4, 5, 7, 1, 9, 6, 3, 2, 8, 4, 5, 7, 1, 9, 6, 3, 2, };
             var sorter = new QuickSort<Int32>();
             sorter.Sort(entries);
             Console.WriteLine(String.Join(" ", entries));
