ef7a951564cdc145ccdfd3844c62d46ad2dde481
Mina <Mina@DESKTOP-GS0U4EE>
2018-02-18 13:54:47 -0600

    Add project files.

---
+++ Program/Program.cs
@@ -0,0 +1,17 @@
+﻿using Sort;
+using System;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var entries = new Int32[] { 8, 4, 5, 7, 1, 9, 6, 3, 2};
+            var sorter = new QuickSort<Int32>();
+            sorter.Sort(entries);
+            Console.WriteLine(String.Join(" ", entries));
+            Console.ReadLine();
+        }
+    }
+}
